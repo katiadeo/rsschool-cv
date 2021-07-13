2e57e020cfd40e73cb04afdf79d7b662ea52d68a
katiadeo <niel.0305@yandex.ru>
2021-07-13 23:10:24 +0300

    feat: add functional burger menu

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // BURGER
+const burger = document.querySelector('.header__container');
+const button = burger.querySelector('.burger');
+
+button.addEventListener('click', event => {
+  burger.classList.toggle('active');
+});
