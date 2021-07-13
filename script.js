4e1032c3ad5a9e41c2216452ab1d8bd534030fe1
katiadeo <niel.0305@yandex.ru>
2021-07-13 23:43:30 +0300

    feat: add navbar disappear on scroll

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const button = burger.querySelector('.burger');
 button.addEventListener('click', event => {
   burger.classList.toggle('active');
 });
+
+// NAVBAR SCROLL
+const header = document.querySelector('.header');
+let prevScrollposition = window.pageYOffset;
+
+window.onscroll = () => {
+  let currentScrollPosition = window.pageYOffset;
+  if (prevScrollposition > currentScrollPosition) {
+    header.style.top = "0";
+  } else {
+    header.style.top = "-120px";
+    burger.classList.remove('active');
+  }
+  prevScrollposition = currentScrollPosition;
+}
